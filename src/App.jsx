b2116f38101947244250deac834eb9da939deb61
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2024-10-25 00:21:44 +0545

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import NotesApp from './components/NotesApp'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <NotesApp />
+    </div>
+  )
+}
+
+export default App
