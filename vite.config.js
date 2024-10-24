6609f28f9f00c918f58eee68fa26189e781218ba
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2024-10-25 00:40:40 +0545

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+import path from "path"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/ournote.git.io/',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
